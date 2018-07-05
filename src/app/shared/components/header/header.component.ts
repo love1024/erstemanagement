da9112e936f0f005490d9a5f466a20396929b781
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2018-07-05 11:55:13 +0200

    check header service

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -22,9 +22,9 @@ export class HeaderComponent implements OnInit {
     const el = document.getElementsByTagName('i')[0];
     console.log(el);
     if (el.classList.contains('rotate')) {
-      this.renderer.removeClass(el, 'rotate');
+    //   this.renderer.removeClass(el, 'rotate');
     } else {
-      this.renderer.addClass(el, 'rotate');
+    //   this.renderer.addClass(el, 'rotate');
     }
   }
 
