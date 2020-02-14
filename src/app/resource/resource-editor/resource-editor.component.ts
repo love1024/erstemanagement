5c7fd13b5e7e45ba428cc03f76338155c8e83465
lovepreetsingh <kaler1024@gmail.com>
2020-02-14 14:37:35 +0530

    updated

--- src/app/resource/resource-editor/resource-editor.component.ts
+++ src/app/resource/resource-editor/resource-editor.component.ts
@@ -1,148 +1,153 @@
-import { Component, OnInit, Inject, OnChanges, Input, Output, EventEmitter, Renderer2 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Resource } from '../../shared/models/admin/resource.model';
-import { trigger, transition, style, animate, state } from '@angular/animations';
-import { SnackbarService } from '../../core/snackbar/snackbar.service';
-import { IUser } from 'src/app/shared/models/user/user';
-import { UserService } from 'src/app/core/user/user.service';
-import { LoginService } from 'src/app/core/login/login.service';
-import { IUserAccessDto } from 'src/app/shared/models/access/access';
-
+import {
+  Component,
+  OnInit,
+  Inject,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter,
+  Renderer2
+} from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Resource } from "../../shared/models/admin/resource.model";
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state
+} from "@angular/animations";
+import { SnackbarService } from "../../core/snackbar/snackbar.service";
+import { IUser } from "src/app/shared/models/user/user";
+import { UserService } from "src/app/core/user/user.service";
+import { LoginService } from "src/app/core/login/login.service";
+import { IUserAccessDto } from "src/app/shared/models/access/access";
 
 @Component({
-    selector: 'erste-resource-editor',
-    templateUrl: './resource-editor.component.html',
-    styleUrls: ['./resource-editor.component.scss'],
-    animations: [
-        trigger('smoothInOut', [
-            state('in', style({ 'height': 'auto' })),
-            transition('void => *', [
-                style({ 'height': '0' }),
-                animate(200)
-            ]),
-            transition('* => void', [
-                animate(225, style({ 'height': '0' }))
-            ])
-        ])
-    ]
+  selector: "erste-resource-editor",
+  templateUrl: "./resource-editor.component.html",
+  styleUrls: ["./resource-editor.component.scss"],
+  animations: [
+    trigger("smoothInOut", [
+      state("in", style({ height: "auto" })),
+      transition("void => *", [style({ height: "0" }), animate(200)]),
+      transition("* => void", [animate(225, style({ height: "0" }))])
+    ])
+  ]
 })
 export class ResourceEditorComponent implements OnInit, OnChanges {
+  inputForm: FormGroup;
 
-    inputForm: FormGroup;
+  @Input() user: IUser;
+  @Input() isNew: Boolean;
+  @Input() expanded: Boolean;
+  @Output() refresh = new EventEmitter();
 
-    @Input() user: IUser;
-    @Input() isNew: Boolean;
-    @Input() expanded: Boolean;
-    @Output() refresh = new EventEmitter();
+  super: IUser;
 
-    super: IUser;
+  userAccess: IUserAccessDto;
 
-    userAccess: IUserAccessDto;
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackbarService: SnackbarService,
+    private userService: UserService,
+    private loginService: LoginService,
+    private renderer: Renderer2
+  ) {
+    this.super = this.loginService.getUser();
+  }
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private snackbarService: SnackbarService,
-        private userService: UserService,
-        private loginService: LoginService,
-        private renderer: Renderer2) { 
-        this.super = this.loginService.getUser();
+  ngOnChanges() {
+    if (this.user) {
+      this.createEditForm(this.user);
+    } else {
+      this.createForm();
     }
-
-
-    ngOnChanges() {
-        if (this.user) {
-            this.createEditForm(this.user);
-        } else {
-            this.createForm();
-        }
-        if (this.isNew) {
-            let container = document.getElementById('form-container');
-            this.renderer.setStyle(container, 'margin-bottom', '50px');
-            this.renderer.setStyle(container, 'border', '1px solid lightgrey')
-            this.renderer.setStyle(container, 'box-shadow', 'none')
-        }
-        this.userService.getUserAllAccess(this.super.userId).subscribe((access) => {
-            this.userAccess = access.userAccess;
-            if(this.super.role != 'Business-User') {
-                this.userAccess.create = this.userAccess.delete = this.userAccess.edit = this.userAccess.view = true;
-            }
-        });
+    if (this.isNew) {
+      let container = document.getElementById("form-container");
+      this.renderer.setStyle(container, "margin-bottom", "50px");
+      this.renderer.setStyle(container, "border", "1px solid lightgrey");
+      this.renderer.setStyle(container, "box-shadow", "none");
     }
+    this.userService.getUserAllAccess(this.super.userId).subscribe(access => {
+      this.userAccess = access.userAccess;
+      if (this.super.role != "Professor") {
+        this.userAccess.create = this.userAccess.delete = this.userAccess.edit = this.userAccess.view = true;
+      }
+    });
+  }
 
-    ngOnInit() {
+  ngOnInit() {}
 
+  createForm(): void {
+    this.inputForm = this.formBuilder.group({
+      userId: [""],
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      emailVerified: ["", [Validators.required]],
+      emailAddress: ["", [Validators.required]],
+      role: ["User", [Validators.required]],
+      maxUsers: [10, [Validators.required]]
+    });
+  }
+
+  createEditForm(user: IUser): void {
+    this.inputForm = this.formBuilder.group({
+      userId: [user.userId, [Validators.required]],
+      firstName: [user.firstName, [Validators.required]],
+      lastName: [user.lastName, [Validators.required]],
+      emailVerified: [user.emailVerified, [Validators.required]],
+      emailAddress: [user.emailAddress, [Validators.required]],
+      role: [user.role, [Validators.required]],
+      maxUsers: [user.maxUsers, [Validators.required]]
+    });
+  }
+
+  onSubmit(isValid: boolean) {
+    if (isValid) {
+      if (this.user) {
+        this.updateResource(this.inputForm.value);
+      } else {
+        this.createResource(this.inputForm.value);
+      }
     }
+  }
 
-    createForm(): void {
-        this.inputForm = this.formBuilder.group({
-            userId: [''],
-            firstName: ['', [Validators.required]],
-            lastName: ['', [Validators.required]],
-            emailVerified: ['', [Validators.required]],
-            emailAddress: ['', [Validators.required]],
-            role: ['User', [Validators.required]],
-            maxUsers: [10, [Validators.required]]
-        });
+  onDelete() {
+    this.deleteResource(this.user.userId);
+  }
+
+  updateResource(user: IUser): void {
+    const userId = this.loginService.getUser().userId;
+    this.userService.updateUser(user, userId).subscribe(res => {
+      this.snackbarService.open("User Saved Successfully");
+      this.emitRefresh();
+    });
+  }
+
+  createResource(user: IUser): void {
+    const userId = this.loginService.getUser().userId;
+    this.userService.addUser(user, userId).subscribe(res => {
+      this.snackbarService.open("User Added Successfully");
+      this.emitRefresh();
+    });
+  }
+
+  deleteResource(id): void {
+    const userId = this.loginService.getUser().userId;
+    this.userService.deleteUser(id, userId).subscribe(res => {
+      this.emitRefresh();
+    });
+  }
+
+  emitRefresh() {
+    this.refresh.emit("");
+  }
+
+  checkDefined(resource: IUser): boolean {
+    if (resource != null && resource !== undefined) {
+      return true;
     }
-
-    createEditForm(user: IUser): void {
-        this.inputForm = this.formBuilder.group({
-            userId: [user.userId, [Validators.required]],
-            firstName: [user.firstName, [Validators.required]],
-            lastName: [user.lastName, [Validators.required]],
-            emailVerified: [user.emailVerified, [Validators.required]],
-            emailAddress: [user.emailAddress, [Validators.required]],
-            role: [user.role, [Validators.required]],
-            maxUsers: [user.maxUsers, [Validators.required]]
-        });
-    }
-
-    onSubmit(isValid: boolean) {
-        if (isValid) {
-            if (this.user) {
-                this.updateResource(this.inputForm.value);
-            } else {
-                this.createResource(this.inputForm.value);
-            }
-        }
-    }
-
-    onDelete() {
-        this.deleteResource(this.user.userId);
-    }
-
-
-    updateResource(user: IUser): void {
-        const userId = this.loginService.getUser().userId;
-        this.userService.updateUser(user, userId).subscribe(res => {
-            this.snackbarService.open("User Saved Successfully");
-            this.emitRefresh()
-        });
-    }
-
-    createResource(user: IUser): void {
-        const userId = this.loginService.getUser().userId;
-        this.userService.addUser(user,userId).subscribe((res) => {
-            this.snackbarService.open("User Added Successfully");
-            this.emitRefresh()   
-        });
-    }
-
-    deleteResource(id): void {
-        const userId = this.loginService.getUser().userId;
-        this.userService.deleteUser(id, userId).subscribe(res => {
-            this.emitRefresh()
-        });
-    }
-
-    emitRefresh() {
-        this.refresh.emit('');
-    }
-
-    checkDefined(resource: IUser): boolean {
-        if (resource != null && resource !== undefined) {
-            return true;
-        }
-        return false;
-    }
+    return false;
+  }
 }
--- src/app/resource/resources/resources.component.ts
+++ src/app/resource/resources/resources.component.ts
@@ -1,141 +1,159 @@
-import { MatTableDataSource, MatDialog } from '@angular/material';
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { trigger, transition, style, animate, state } from '@angular/animations';
-import { LoginService } from '../../core/login/login.service';
-import { UserService } from 'src/app/core/user/user.service';
-import { IUser } from 'src/app/shared/models/user/user';
-import { IUserAccessDto } from 'src/app/shared/models/access/access';
+import { MatTableDataSource, MatDialog } from "@angular/material";
+import { Component, OnInit, Renderer2 } from "@angular/core";
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state
+} from "@angular/animations";
+import { LoginService } from "../../core/login/login.service";
+import { UserService } from "src/app/core/user/user.service";
+import { IUser } from "src/app/shared/models/user/user";
+import { IUserAccessDto } from "src/app/shared/models/access/access";
 
 @Component({
-    selector: 'erste-resources',
-    templateUrl: './resources.component.html',
-    styleUrls: ['./resources.component.scss'],
-    animations: [
-        trigger('smoothInOut', [
-            state('in', style({ 'min-height': '30px', 'height': 'auto' })),
-            transition('void => *', [
-                style({ 'min-height': '0', 'height': '0' }),
-                animate(200)
-            ]),
-            transition('* => void', [
-                animate(200, style({ 'height': '0' }))
-            ])
-        ]),
-        trigger('fadeOut', [
-            transition('* => void', [
-                animate(200, style({ 'height': '0' }))
-            ])
-        ]),
-        trigger('detailExpand', [
-            state('collapsed', style({ height: '0px', minHeight: '0', })),
-            state('expanded', style({ height: '*' })),
-            transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-        ]),
-    ]
+  selector: "erste-resources",
+  templateUrl: "./resources.component.html",
+  styleUrls: ["./resources.component.scss"],
+  animations: [
+    trigger("smoothInOut", [
+      state("in", style({ "min-height": "30px", height: "auto" })),
+      transition("void => *", [
+        style({ "min-height": "0", height: "0" }),
+        animate(200)
+      ]),
+      transition("* => void", [animate(200, style({ height: "0" }))])
+    ]),
+    trigger("fadeOut", [
+      transition("* => void", [animate(200, style({ height: "0" }))])
+    ]),
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      )
+    ])
+  ]
 })
 export class ResourcesComponent implements OnInit {
+  displayedColumns = [
+    "userid",
+    "firstname",
+    "lastname",
+    "emailaddress",
+    "emailverified",
+    "role"
+  ];
+  dataSource = new MatTableDataSource();
+  currentUser: IUser;
+  buttonMessage = "Create";
+  isOpen = false;
+  isLoading = true;
 
-    displayedColumns = ['userid', 'firstname', 'lastname', 'emailaddress', 'emailverified', 'role'];
-    dataSource = new MatTableDataSource();
-    currentUser: IUser;
-    buttonMessage = "Create";
-    isOpen = false;
-    isLoading = true;
+  access: IUserAccessDto;
 
-    access: IUserAccessDto;
+  super: IUser;
 
-    super: IUser;
-
-    get canCreate(): boolean {
-        if(!this.access.create) {
-            return false;
-        }
-        if(this.super.role == 'Business' || this.super.role == 'Business-User') {
-            if(this.super) {
-                return this.super.addedUsers < this.super.maxUsers;
-            }
-        }
-        return true;
+  get canCreate(): boolean {
+    if (!this.access.create) {
+      return false;
     }
-
-    constructor(
-        private userSerivce: UserService,
-        private loginService: LoginService,
-        private renderer: Renderer2
-    ) { }
-
-    ngOnInit() {
-        this.super = this.loginService.getUser();
-        this.refreshDataTable();
+    if (this.super.role == "Institute" || this.super.role == "Professor") {
+      if (this.super) {
+        return this.super.addedUsers < this.super.maxUsers;
+      }
     }
+    return true;
+  }
 
-    refreshDataTable() {
-        this.isLoading = true;
-        const userId = this.loginService.getUser().userId;
-        this.userSerivce.getUserInfo(userId).subscribe((user) => {
-            this.super = user;
-        });
-        this.userSerivce.getAllUsers(userId).subscribe((users: IUser[]) => {
-            this.dataSource = new MatTableDataSource(this.addDetailColumn(users));
-            this.dataSource.filterPredicate = this.dataFilter;
-            this.isLoading = false;
-        });
-        this.userSerivce.getUserAllAccess(this.super.userId).subscribe((access) => {
-            this.access = access.userAccess;
-            if(this.super.role != 'Business-User') {
-                this.access.create = this.access.delete = this.access.edit = this.access.view = true;
-            }
-        });
-    }
+  constructor(
+    private userSerivce: UserService,
+    private loginService: LoginService,
+    private renderer: Renderer2
+  ) {}
 
-    toggleDialog() {
-        this.isOpen = !this.isOpen;
-    }
+  ngOnInit() {
+    this.super = this.loginService.getUser();
+    this.refreshDataTable();
+  }
 
-    openDialog() {
-        let icon = document.getElementsByClassName('fa-plus')[0];
-        this.buttonMessage = 'Close';
-        this.renderer.setStyle(icon, 'transform', 'rotate(45deg)')
-        this.isOpen = true;
-    }
+  refreshDataTable() {
+    this.isLoading = true;
+    const userId = this.loginService.getUser().userId;
+    this.userSerivce.getUserInfo(userId).subscribe(user => {
+      this.super = user;
+    });
+    this.userSerivce.getAllUsers(userId).subscribe((users: IUser[]) => {
+      this.dataSource = new MatTableDataSource(this.addDetailColumn(users));
+      this.dataSource.filterPredicate = this.dataFilter;
+      this.isLoading = false;
+    });
+    this.userSerivce.getUserAllAccess(this.super.userId).subscribe(access => {
+      this.access = access.userAccess;
+      if (this.super.role != "Professor") {
+        this.access.create = this.access.delete = this.access.edit = this.access.view = true;
+      }
+    });
+  }
 
-    closeDialog() {
-        let icon = document.getElementsByClassName('fa-plus')[0];
-        this.buttonMessage = 'Entry';
-        this.renderer.setStyle(icon, 'transform', 'rotate(0deg)')
-        this.isOpen = false;
-    }
+  toggleDialog() {
+    this.isOpen = !this.isOpen;
+  }
 
-    isExpansionDetailRow(i: number, row: Object) {
-        return row.hasOwnProperty('editor');
-    }
+  openDialog() {
+    let icon = document.getElementsByClassName("fa-plus")[0];
+    this.buttonMessage = "Close";
+    this.renderer.setStyle(icon, "transform", "rotate(45deg)");
+    this.isOpen = true;
+  }
 
-    onRowClick(user: IUser, i: number) {
-        this.dataSource.data.forEach(data => { data["expanded"] = false });
-        if (user == this.currentUser)
-            this.currentUser = null;
-        else {
-            this.currentUser = user;
-            this.dataSource.data[i + 1]["expanded"] = true;
-        }
-    }
+  closeDialog() {
+    let icon = document.getElementsByClassName("fa-plus")[0];
+    this.buttonMessage = "Entry";
+    this.renderer.setStyle(icon, "transform", "rotate(0deg)");
+    this.isOpen = false;
+  }
 
-    addDetailColumn(list: IUser[]) {
-        const rows = [];
-        list.forEach(element => rows.push(element, { editor: true, expanded: false, element }));
-        return rows;
-    }
+  isExpansionDetailRow(i: number, row: Object) {
+    return row.hasOwnProperty("editor");
+  }
 
-    dataFilter(data: any, filter) {
-        const filterArr = JSON.parse(filter);
-        const str = filterArr.val.toString().toLowerCase();
-        const col = filterArr.col;
-        if (data.editor)
-            return true;
-        return data[col].toString().toLowerCase().indexOf(str) != -1;
+  onRowClick(user: IUser, i: number) {
+    this.dataSource.data.forEach(data => {
+      data["expanded"] = false;
+    });
+    if (user == this.currentUser) this.currentUser = null;
+    else {
+      this.currentUser = user;
+      this.dataSource.data[i + 1]["expanded"] = true;
     }
+  }
 
-    onFilter(col, val) {
-        this.dataSource.filter = JSON.stringify({ col, val });
-    }
+  addDetailColumn(list: IUser[]) {
+    const rows = [];
+    list.forEach(element =>
+      rows.push(element, { editor: true, expanded: false, element })
+    );
+    return rows;
+  }
+
+  dataFilter(data: any, filter) {
+    const filterArr = JSON.parse(filter);
+    const str = filterArr.val.toString().toLowerCase();
+    const col = filterArr.col;
+    if (data.editor) return true;
+    return (
+      data[col]
+        .toString()
+        .toLowerCase()
+        .indexOf(str) != -1
+    );
+  }
+
+  onFilter(col, val) {
+    this.dataSource.filter = JSON.stringify({ col, val });
+  }
 }
